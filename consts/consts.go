f9b87c0b3a69904e7008f9d7c1fc748091dc27d1
k0pernicus <antonin.carette@gmail.com>
2017-04-13 00:38:19 +0200

    Add consts package

---
+++ consts/consts.go
@@ -0,0 +1,7 @@
+/*
+Package consts implements constants for the entire project
+*/
+package consts
+
+// ConfigurationFileName is the configuration file name of Goyave
+const ConfigurationFileName = ".goyave.toml"
