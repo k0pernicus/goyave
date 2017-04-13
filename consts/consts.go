4b6a948ecf7aef05db618e5e82fbe323f707182c
k0pernicus <antonin.carette@gmail.com>
2017-04-13 13:32:56 +0200

    Add the name of a Git directory

--- consts/consts.go
+++ consts/consts.go
@@ -5,3 +5,6 @@ package consts
 
 // ConfigurationFileName is the configuration file name of Goyave
 const ConfigurationFileName = ".goyave"
+
+// GitFileName is the name of the git directory, in a git repository
+const GitFileName = ".git"
