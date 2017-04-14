1ab5d9bb31310f8afe9f04b1c184da8eab170c7b
k0pernicus <antonin.carette@gmail.com>
2017-04-14 22:25:33 +0200

    Change documentation style

--- consts/consts.go
+++ consts/consts.go
@@ -1,6 +1,5 @@
-/*
-Package consts implements constants for the entire project
-*/
+/*Package consts implements constants for the entire project
+ */
 package consts
 
 // ConfigurationFileName is the configuration file name of Goyave
@@ -8,3 +7,10 @@ const ConfigurationFileName = ".goyave"
 
 // GitFileName is the name of the git directory, in a git repository
 const GitFileName = ".git"
+
+////////////////
+//// ERRORS ////
+////////////////
+
+// Error when append an existing path repository in a list
+const RepositoryAlreadyExists = "REPOSITORY_ALREADY_EXISTS"
