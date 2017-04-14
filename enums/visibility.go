1130307b546d4228f1aeee3876fa41fa60a32578
k0pernicus <antonin.carette@gmail.com>
2017-04-14 22:24:41 +0200

    Change documentation style

--- enums/visibility.go
+++ enums/visibility.go
@@ -1,21 +1,18 @@
-/*
-Package enums implements enums for the entire project
-*/
+/*Package enums implements enums for the entire project
+ */
 package enums
 
 // VISIBILITY is a string alias to get the local visibility of git repositories
 type VISIBILITY string
 
-/*
-Enumeration to specify the local visibility of git repositories
-
-Those enumerations are:
-
-	VISIBLE:
-		Visible repository - can be access through the program
-	HIDDEN:
-		An ignored repository - canno't be access through the program
-*/
+/*Enumeration to specify the local visibility of git repositories
+ *
+ *Those enumerations are:
+ *	VISIBLE:
+ *		Visible repository - can be access through the program
+ *	HIDDEN:
+ *		An ignored repository - canno't be access through the program
+ */
 const (
 	VISIBLE = VISIBILITY("VISIBLE")
 	HIDDEN  = VISIBILITY("HIDDEN")
