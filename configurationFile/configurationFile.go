8dada8241c1fb9ef125c9af4c11a76615f2cb329
k0pernicus <antonin.carette@gmail.com>
2017-04-20 23:49:16 +0200

    Refactor some configurationFile functions or methods

--- configurationFile/configurationFile.go
+++ configurationFile/configurationFile.go
@@ -7,13 +7,16 @@ package configurationFile
 
 import (
 	"bytes"
-
 	"errors"
-
 	"path/filepath"
+
+	"fmt"
+
+	"os/user"
 
 	"github.com/BurntSushi/toml"
 	"github.com/k0pernicus/goyave/consts"
+	"github.com/k0pernicus/goyave/utils"
 )
 
 /*ConfigurationFile represents the TOML structure of the Goyave configuration file.
@@ -36,38 +39,76 @@ type ConfigurationFile struct {
 	Groups              []Group           `toml:"group"`
 }
 
-/*AddVisibleRepository adds a given git repo path as a visible repository
+/*Default returns a default ConfigurationFile structure.
+ */
+func Default() *ConfigurationFile {
+	usr, _ := user.Current()
+	return &ConfigurationFile{
+		Author: usr.Username,
+		Local: LocalInformations{
+			DefaultEntry: consts.VisibleFlag,
+			Group:        utils.GetLocalhost(),
+		},
+	}
+}
+
+/*GetDefaultEntry returns the default entry to store a new git repository.
+ *This methods returns HiddenFlag, or VisibleFlag
+ */
+func (c *ConfigurationFile) GetDefaultEntry() (string, error) {
+	defaultEntry := c.Local.DefaultEntry
+	if defaultEntry != consts.VisibleFlag || defaultEntry != consts.HiddenFlag {
+		return consts.VisibleFlag, fmt.Errorf("the default entry is not set to %s or %s. Please check your configuration file", consts.HiddenFlag, consts.VisibleFlag)
+	}
+	return defaultEntry, nil
+}
+
+/*AddRepository will add a single path in the TOML's target if the path does not exists.
+ *
+ *This method uses both methods addVisibleRepository and addHiddenRepository.
+ */
+func (c *ConfigurationFile) AddRepository(path string, target string) error {
+	if target == consts.VisibleFlag {
+		return c.addVisibleRepository(path)
+	}
+	if target == consts.HiddenFlag {
+		return c.addHiddenRepository(path)
+	}
+	return errors.New("The target does not exists.")
+}
+
+/*addVisibleRepository adds a given git repo path as a visible repository
  *
  *If the repository already exists in the VisibleRepository field, the method throws an error: RepositoryAlreadyExists.
  *Else, the repository is append to the VisibleRepository field, and the method returns nil.
  */
-func (c *ConfigurationFile) AddVisibleRepository(path string) error {
+func (c *ConfigurationFile) addVisibleRepository(path string) error {
 	for _, registeredRepository := range c.VisibleRepositories {
 		if registeredRepository.Path == path {
 			return errors.New(consts.RepositoryAlreadyExists)
 		}
 	}
 	var newVisibleRepository = GitRepository{
-		Name: filepath.Ext(path),
+		Name: filepath.Base(path),
 		Path: path,
 	}
 	c.VisibleRepositories = append(c.VisibleRepositories, newVisibleRepository)
 	return nil
 }
 
-/*AddHiddenRepository adds a given git repo path as an hidden repository
+/*addHiddenRepository adds a given git repo path as an hidden repository
  *
  *If the repository already exists in the HiddenRepository field, the method throws an error: RepositoryAlreadyExists.
  *Else, the repository is append to the HiddenRepository field, and the method returns nil.
  */
-func (c *ConfigurationFile) AddHiddenRepository(path string) error {
+func (c *ConfigurationFile) addHiddenRepository(path string) error {
 	for _, registeredRepository := range c.HiddenRepositories {
 		if registeredRepository.Path == path {
 			return errors.New(consts.RepositoryAlreadyExists)
 		}
 	}
 	var newHiddenRepository = GitRepository{
-		Name: filepath.Ext(path),
+		Name: filepath.Base(path),
 		Path: path,
 	}
 	c.HiddenRepositories = append(c.HiddenRepositories, newHiddenRepository)
@@ -104,22 +145,32 @@ type Group struct {
 /*LocalInformations represents your local configuration of Goyave.
  *
  *The structure contains:
+ *  DefaultEntry:
+ *		The default entry to store a git repository (hidden or visible).
  *	Group:
  *		The current group name.
  */
 type LocalInformations struct {
-	Group string
+	DefaultEntry string
+	Group        string
 }
 
-/*Decode is a function to decode an entire string (which is the content of a given TOML file) to a ConfigurationFile structure.
+/*DecodeString is a function to decode an entire string (which is the content of a given TOML file) to a ConfigurationFile structure.
  */
-func Decode(data string, localStructure *ConfigurationFile) error {
-	_, err := toml.Decode(data, localStructure)
+func (c *ConfigurationFile) DecodeString(data string) error {
+	_, err := toml.Decode(data, c)
+	return err
+}
+
+/*DecodeBytesArray is a function to decode an entire string (which is the content of a given TOML file) to a ConfigurationFile structure.
+ */
+func (c *ConfigurationFile) DecodeBytesArray(data []byte) error {
+	_, err := toml.Decode(string(data[:]), c)
 	return err
 }
 
 /*Encode is a function to encode a ConfigurationFile structure to a byffer of bytes.
  */
-func Encode(localStructure *ConfigurationFile, buffer *bytes.Buffer) error {
-	return toml.NewEncoder(buffer).Encode(localStructure)
+func (c *ConfigurationFile) Encode(buffer *bytes.Buffer) error {
+	return toml.NewEncoder(buffer).Encode(c)
 }
