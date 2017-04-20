c6a6a0cfc1752b48763d764c92b8caf74b0cf522
k0pernicus <antonin.carette@gmail.com>
2017-04-20 23:47:22 +0200

    Add a function to get the localhost

--- utils/utils.go
+++ utils/utils.go
@@ -15,3 +15,14 @@ func GetUserHomeDir() string {
 	}
 	return usr.HomeDir
 }
+
+/*GetLocalhost returns the localhost name of the current computer.
+ *If there is an error, it returns a default string.
+ */
+func GetLocalhost() string {
+	lhost, err := os.Hostname()
+	if err != nil {
+		return "DefaultHostname"
+	}
+	return lhost
+}
