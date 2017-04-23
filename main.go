a58e029de98b36ffe1ec5469d886c42318b5d280
k0pernicus <antonin.carette@gmail.com>
2017-04-23 15:55:38 +0200

    Usage of Cobra, to build a CLI

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -12,38 +13,28 @@ import (
 	"github.com/k0pernicus/goyave/consts"
 	"github.com/k0pernicus/goyave/traces"
 	"github.com/k0pernicus/goyave/utils"
+	"github.com/k0pernicus/goyave/walk"
+	"github.com/spf13/cobra"
 )
 
-/*getConfigurationFileContent get the content of the local configuration file.
- *If no configuration file has been found, create a default one and set the bytes array.
+var configurationFileStructure configurationFile.ConfigurationFile
+var configurationFilePath string
+var userHomeDir string
+
+/*initialize get the configuration file existing in the system (or create it), and return
+ *a pointer to his content.
  */
-func getConfigurationFileContent(filePointer *os.File, bytesArray *[]byte) {
-	fileState, err := filePointer.Stat()
-	// If the file is empty, get the default structure and save it
-	if err != nil || fileState.Size() == 0 {
-		traces.WarningTracer.Println("No (or empty) configuration file - creating default one...")
-		var fileBuffer bytes.Buffer
-		defaultStructure := configurationFile.Default()
-		defaultStructure.Encode(&fileBuffer)
-		*bytesArray = fileBuffer.Bytes()
-	} else {
-		b, _ := ioutil.ReadAll(filePointer)
-		*bytesArray = b
-	}
-}
-
-func main() {
-
+func initialize(configurationFileStructure *configurationFile.ConfigurationFile) {
 	// Initialize all different traces structures
 	traces.InitTraces(os.Stdout, os.Stderr, os.Stdout, os.Stdout)
 
 	// Get the user home directory
-	userHomeDir := utils.GetUserHomeDir()
+	userHomeDir = utils.GetUserHomeDir()
 	if len(userHomeDir) == 0 {
 		log.Fatalf("Cannot get the user home dir.\n")
 	}
 	// Set the configuration path file
-	configurationFilePath := path.Join(userHomeDir, consts.ConfigurationFileName)
+	configurationFilePath = path.Join(userHomeDir, consts.ConfigurationFileName)
 	filePointer, err := os.OpenFile(configurationFilePath, os.O_RDWR|os.O_CREATE, 0755)
 	if err != nil {
 		log.Fatalf("Cannot open the file %s, due to error '%s'.\n", configurationFilePath, err)
@@ -51,34 +42,71 @@ func main() {
 	defer filePointer.Close()
 	var bytesArray []byte
 	// Get the content of the goyave configuration file
-	getConfigurationFileContent(filePointer, &bytesArray)
-	var configurationFileStructure configurationFile.ConfigurationFile
-	if _, err = toml.Decode(string(bytesArray[:]), &configurationFileStructure); err != nil {
+	configurationFile.GetConfigurationFileContent(filePointer, &bytesArray)
+	if _, err = toml.Decode(string(bytesArray[:]), configurationFileStructure); err != nil {
 		log.Fatalln(err)
 	}
-	// Get all git paths, and display them
-	// gitPaths, err := walk.RetrieveGitRepositories(userHomeDir)
-	// if err != nil {
-	// 	log.Fatalf("There was an error retrieving your git repositories: '%s'\n", err)
-	// }
-	// // For each git repository, check if it exists, and if not add it to the default target visibility
-	// for _, gitPath := range gitPaths {
-	// 	if err := configurationFileStructure.AddRepository(gitPath, configurationFileStructure.Local.DefaultTarget); err != nil {
-	// 		traces.WarningTracer.Printf("[%s] %s", gitPath, err)
-	// 	}
-	// }
-	// For each VISIBLE repository, get some informations about his state and display it
-	for _, gitStruct := range configurationFileStructure.VisibleRepositories {
-		// Add those instruction in goroutines
-		gitStruct.Init()
-		gitStruct.GitObject.Status()
+}
+
+func main() {
+
+	var rootCmd = &cobra.Command{
+		Use:   "goyave",
+		Short: "Goyave is a tool to take a look at your local git repositories",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			initialize(&configurationFileStructure)
+		},
+		PersistentPostRun: func(cmd *cobra.Command, args []string) {
+			var outputBuffer bytes.Buffer
+			if err := configurationFileStructure.Encode(&outputBuffer); err != nil {
+				log.Fatalln("Cannot save the current configurationFile structure!")
+			}
+			if err := ioutil.WriteFile(configurationFilePath, outputBuffer.Bytes(), 0777); err != nil {
+				log.Fatalln("Cannot access to your file to save the configurationFile structure!")
+			}
+		},
 	}
-	// Save all new changes
-	var outputBuffer bytes.Buffer
-	if err := configurationFileStructure.Encode(&outputBuffer); err != nil {
-		log.Fatalln("Cannot save the current configurationFile structure!")
+
+	/*stateCmd is a subcommand to list the state of each local git repository
+	 */
+	var stateCmd = &cobra.Command{
+		Use:   "state",
+		Short: "Get the state of each local git repository",
+		Run: func(cmd *cobra.Command, args []string) {
+			for _, gitStruct := range configurationFileStructure.VisibleRepositories {
+				gitStruct.Init()
+				gitStruct.GitObject.Status()
+			}
+		},
 	}
-	if err := ioutil.WriteFile(configurationFilePath, outputBuffer.Bytes(), 0777); err != nil {
-		log.Fatalln("Cannot access to your file to save the configurationFile structure!")
+
+	/*crawlCmd is a subcommand to crawl your hard drive in order to get and save new git repositories
+	 */
+	var crawlCmd = &cobra.Command{
+		Use:   "crawl",
+		Short: "Crawl the hard drive in order to find new git repositories",
+		Run: func(cmd *cobra.Command, args []string) {
+			//		Get all git paths, and display them
+			gitPaths, err := walk.RetrieveGitRepositories(userHomeDir)
+			if err != nil {
+				log.Fatalf("There was an error retrieving your git repositories: '%s'\n", err)
+			}
+			// For each git repository, check if it exists, and if not add it to the default target visibility
+			for _, gitPath := range gitPaths {
+				if err := configurationFileStructure.AddRepository(gitPath, configurationFileStructure.Local.DefaultTarget); err != nil {
+					traces.WarningTracer.Printf("[%s] %s", gitPath, err)
+				}
+			}
+			//For each VISIBLE repository, get some informations about his state and display it
+		},
 	}
+
+	rootCmd.AddCommand(crawlCmd, stateCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// // Save all new changes
 }
